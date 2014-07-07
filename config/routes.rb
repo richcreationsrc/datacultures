32c81c1023fd3db96a8dc91820ab7732bb03ed3d
Craig A. Cook <coyot3@berkeley.edu>
2014-07-07 12:27:26 -0700

    CYB-50 Adding API infrastructure.  Enscapsulates all calls to the Canvas API in this new class
      * Uses Faraday & Net::HTTP::Persistent (keeps connections open, to save setup/teardown)
      * Requires setup in config/secrets.yml with API keys from Canvas (see README.md)
      * Some specs do actual calls, so for them to run it requires an instance of Canvas.
      * Some rewrite on README.md

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,5 @@ Rails.application.routes.draw do
   end
   resources :points, only: [:index, :show, :create, :update]
 
-  get '/*url' => 'bootstrap#index', :defaults => { :format => 'html' }
+  #get '/*url' => 'bootstrap#index', :defaults => { :format => 'html' }
 end
