b9655c2b72a38310ac6954e04e4049cb64510653
Richard Chen <rlc11752@gmail.com>
2014-07-08 16:20:44 -0700

    [Points_Configuration] Updated html, css, and js files for points configuration via Christian's suggestions

--- app/assets/javascripts/angular/controllers/pages/pointsConfigurationController.js
+++ app/assets/javascripts/angular/controllers/pages/pointsConfigurationController.js
@@ -4,25 +4,19 @@
   'use strict';
 
   angular.module('datacultures.controllers').controller('PointsConfigurationController', function($http, $scope) {
-
-    $http.get('/dummy/json/activities.json').success(function(data) {
-      angular.extend($scope, data);
+    $http.get('/dummy/json/activities.json').success(function(activities) {
+      angular.extend($scope, activities);
     });
 
-    $scope.activities = $scope.data;
-    $scope.activityForm = false;
     $scope.removedTable = false;
-    $scope.isHidden = true;
-    $scope.master = {};
-    $scope.pointTotalArray = [0,0,0,0,0,0,0,0,0,0,0];
+    $scope.pointTotalArray = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
     $scope.removedActivitiesList = [];
-    $scope.initialPoints = 0;
-
+    var master = {};
     $scope.status = 'uneditable';
 
     $scope.save = function() {
       $scope.status = 'uneditable';
-      for (var i = 0; i<$scope.pointTotalArray.length; i++) {
+      for (var i = 0; i < $scope.pointTotalArray.length; i++) {
         if ($scope.pointTotalArray[i] === '' || $scope.pointTotalArray[i] === null) {
           $scope.activities[i].points = 0;
         } else {
@@ -40,27 +34,26 @@
     };
 
     $scope.deleteActivity = function(activity) {
-      $scope.master = angular.copy(activity);
+      master = angular.copy(activity);
       $scope.removedTable = true;
 
-      $scope.pointTotalArray.splice($scope.activities.indexOf(activity),1); // remove from totalpoints array
-      $scope.activities.splice($scope.activities.indexOf(activity),1); // remove from JSON object
+      $scope.pointTotalArray.splice($scope.activities.indexOf(activity), 1); // remove from totalpoints array
+      $scope.activities.splice($scope.activities.indexOf(activity), 1); // remove from JSON object
 
       $scope.removedActivitiesList.push(activity); // push onto removed activities list
     };
 
     $scope.update = function(activity) {
-      $scope.master = angular.copy(activity);
-      $scope.insertBack(activity, $scope.master);
-      $scope.removedActivitiesList.splice($scope.removedActivitiesList.indexOf(activity),1);
+      master = angular.copy(activity);
+      $scope.insertBack(activity, master);
+      $scope.removedActivitiesList.splice($scope.removedActivitiesList.indexOf(activity), 1);
     };
 
     $scope.insertBack = function(activity, masterTask) {
       for (var i = 0; i<$scope.pointTotalArray.length; i++) {
-        // alert($scope.pointTotalArray[i].id);
         if (activity.id < $scope.activities[i].id) {
-          $scope.pointTotalArray.splice(i,0, masterTask.points);
-          $scope.activities.splice(i,0,masterTask);
+          $scope.pointTotalArray.splice(i, 0, masterTask.points);
+          $scope.activities.splice(i, 0, masterTask);
           return;
         }
       }
