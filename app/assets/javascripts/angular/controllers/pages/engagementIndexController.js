9a213732ed7bf5ca6a97b5237410307c970cf515
Richard Chen <rlc11752@gmail.com>
2014-07-28 17:32:08 -0700

    [no-jira] - Added Landing Page, Modified JSON file, edited index

--- app/assets/javascripts/angular/controllers/pages/engagementIndexController.js
+++ app/assets/javascripts/angular/controllers/pages/engagementIndexController.js
@@ -6,23 +6,39 @@
     $scope.shownote = true;
     $scope.showshare = true;
     $scope.showpoints = false;
+    $scope.visibility = true;
+    $scope.noshowStudents = [];
 
     studentFactory.getStudents().
       success(function(results) {
         $scope.people = results.students;
 
-        for (var i = 0; i<$scope.people.length; i++) {
-          if ($scope.people[i].currentStudent === true) {
-            $scope.currStudent = $scope.people[i];
-            $scope.people.splice($scope.people.indexOf($scope.currStudent), 1);
-        }
 
-          if ($scope.people[i].sharePoints === 'NO') {
+        $scope.currStudent = $scope.people[0];
+        $scope.people.splice($scope.people.indexOf($scope.currStudent), 1);
+
+
+        for (var i = 0; i<$scope.people.length; i++) {
+        //   if ($scope.people[i].currentStudent === true) {
+        //     $scope.currStudent = $scope.people[i];
+        //     $scope.people.splice($scope.people.indexOf($scope.currStudent), 1);
+        // }
+
+          if ($scope.people[i].share === 'NO') {
             $scope.people[i].points = '--';
+            $scope.people[i].lastupdate = '--';
+            // $scope.optOutStudent = $scope.people.splice($scope.people.indexOf($scope.people[i]), 1);
+
+
+            $scope.optOutStudent = $scope.people[i];
+            alert($scope.optOutStudent.points);
+            $scope.noshowStudents.push($scope.optOutStudent);
+            alert($scope.optOutStudent.name);
+
           }
       }
         // Default Sort
-        $scope.predicate = 'position';
+        $scope.predicate = 'points';
     });
 
 
@@ -40,6 +56,10 @@
       $scope.shownote = true;
       $scope.showpoints = false;
     };
+
+
+
+
 
   });
 
