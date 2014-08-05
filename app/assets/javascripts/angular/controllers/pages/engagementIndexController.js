dab278ea07656bd87034b2dce5cbf422bce78b06
Richard Chen <rlc11752@gmail.com>
2014-08-05 15:26:43 -0700

    [nojira] - Added an EI instructor view; WIP for sending share data to backend

--- app/assets/javascripts/angular/controllers/pages/engagementIndexController.js
+++ app/assets/javascripts/angular/controllers/pages/engagementIndexController.js
@@ -3,40 +3,62 @@
 
   angular.module('datacultures.controllers').controller('EngagementIndexController', function($scope, studentFactory, $location) {
 
+    // Define variables for carot symbol flip for sorting
     $scope.showcaretStudent = false;
     $scope.showcaretSection = false;
     $scope.showcaretPoints = false;
     $scope.showcaretLastPoint = false;
-    $scope.score = 'yes'; //sets default radio button to select the 'yes' option
+    $scope.showcaretShare = false;
 
+    // Define variables that deal with filling the EI
     $scope.showshare = true;
     $scope.showpoints = false;
     $scope.visibility = true;
-    $scope.noshowStudents = []; // array that stores all students who are not sharing their score
+    $scope.noshowStudents = []; // array storing students who are not sharing their score
 
+    // Call getStudents() from EI factory to get students
     studentFactory.getStudents().
       success(function(results) {
-        $scope.people = results.students;
-        $scope.currentStudentID = results.current_canvas_user_id;
-        $scope.currStudent = $scope.people[$scope.currentStudentID]; // simulating the first student in json file is the current user
+        $scope.people = results.students; // get from ruby controller
+        $scope.currentStudentID = results.current_canvas_user_id; // get from ruby controller
+        // alert(typeof $scope.currentStudentID);
+        $scope.currStudent = $scope.people[$scope.currentStudentID];
         $scope.shareStatus = $scope.currStudent.share;
 
-        // if user chooses not to share EI, remove them from the shared table
+        // Set current user share status to 'NO' if they choose not to share EI score
+        // They will be removed from the main EI table below
         if ($location.path() === '/engagement_index') {
           $scope.currStudent.share = 'NO';
         }
 
         // Loop through and remove all students that are not sharing Engagement Index
         for (var i = $scope.people.length-1; i >= 0; i--) {
+          if ($scope.people[i].share === true) {
+            $scope.people[i].share = 'YES';
+          }
+
           if ($scope.people[i].share === false) {
             $scope.people[i].share = 'NO';
-            $scope.studentToRemove = $scope.people[i];
-            $scope.noshowStudents.push($scope.studentToRemove); //push not sharing students to new array
-            $scope.people.splice($scope.people.indexOf($scope.studentToRemove), 1); //remove not sharing student
-            continue;
+              if ($location.path() === '/engagement_index_instructor') {
+                continue;
+              } else {
+                $scope.studentToRemove = $scope.people[i]; // store "to be removed" into variable
+                $scope.noshowStudents.push($scope.studentToRemove); // push not sharing students to new array
+                $scope.people.splice($scope.people.indexOf($scope.studentToRemove), 1); // remove not sharing student
+                continue;
+              }
           }
-          $scope.people[i].share = 'YES';
+
+          // Handle row highlighting
+          if ($scope.people[i] === $scope.currStudent) {
+            $scope.people[i].highlight = true;
+          } else {
+            $scope.people[i].highlight = false;
+          }
+          $scope.people[i].share = 'YES'; // if get here, means that the student chose to share EI score
         }
+
+        // Send current student's share status to database
         studentFactory.postStudentStatus($scope.currentStudentID, $scope.shareStatus).
           success(function() {}).
           error(function() {
@@ -47,6 +69,7 @@
         $scope.predicate = 'points';
         $scope.predicateUnshare = 'section';
       });
+
   });
 
 })(window.angular);
--- app/assets/javascripts/angular/controllers/pages/engagementIndexLandingPageController.js
+++ app/assets/javascripts/angular/controllers/pages/engagementIndexLandingPageController.js
@@ -1,15 +1,34 @@
 (function(angular) {
   'use strict';
 
-  angular.module('datacultures.controllers').controller('EngagementIndexLandingPageController', function($scope) {
+  angular.module('datacultures.controllers').controller('EngagementIndexLandingPageController', function($scope, $http) {
 
-    $scope.score = 'yes'; //sets default radio button to select the 'yes' option
+    // $scope.score = 'yes'; //sets default radio button to select the 'yes' option
+    $scope.choice = 'yes';
+
+    // current_canvas_user_id
+
+    // var postStudentStatus = function(canvasUserID, choice1) {
+    //   var url = '/api/v1/students/' + current_canvas_user_id;
+    //   var data = {'status': choice1};
+    //   $http.post(url, data);
+    // };
+
+
+
+
+    // alert(current_canvas_user_id);
 
     $scope.redirectPage = function () {
-      if ($scope.score === 'yes'){
-        return '/engagement_index_share';
-      } else if ($scope.score === 'no'){
-        return('/engagement_index');
+      var url = '/api/v1/students/' + current_canvas_user_id;
+      if ($scope.choice == 'yes'){
+        var data = {'status': $scope.choice1};
+        $http.post(url, data);
+        return ('/engagement_index_share');
+      } else if ($scope.choice2){
+        var data2 = {'status': $scope.choice2};
+        $http.post(url, data2);
+        return ('/engagement_index');
       }
     };
   });
